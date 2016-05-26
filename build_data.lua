82519b0b0993359a5ef3f7e893cef628fb491ca1
Jonas Schievink <jonas@schievink.net>
2016-05-26 19:43:10 +0200

    Add the avr-gcc toolchain

--- build_data.lua
+++ build_data.lua
@@ -56,6 +56,17 @@ local toolchain_list =
     cross_lualong = 'int 32',
     version = '--version'
   },
+  [ 'avr-gcc' ] = {
+    compile = 'avr-gcc',
+    link = 'avr-ld',
+    asm = 'avr-as',
+    bin = 'avr-objcopy',
+    size = 'avr-size',
+    cross_cpumode = 'little',
+    cross_lua = 'float 32',
+    cross_lualong = 'int 32',
+    version = '--version'
+  },
   [ 'avr32-unknown-none-gcc' ] = {
     compile = 'avr32-unknown-none-gcc',
     link = 'avr32-unknown-none-ld',
@@ -84,11 +95,11 @@ local toolchain_list =
 toolchain_list[ 'devkitarm' ] = toolchain_list[ 'arm-eabi-gcc' ]
 
 -- List of acrhitectures and their endianness
-local arch_data = { 
+local arch_data = {
   arm = 'little',
   cortexm = 'little',
   avr32 = 'big',
-  i386 = 'little' 
+  i386 = 'little'
 }
 
 -- Toolchain to arch mapping
@@ -96,6 +107,7 @@ local toolchain_map = {
   arm = { 'arm-gcc', 'codesourcery', 'devkitarm', 'arm-eabi-gcc' },
   cortexm = { 'arm-gcc', 'codesourcery', 'devkitarm', 'arm-eabi-gcc' },
   avr32 = { 'avr32-gcc', 'avr32-unknown-none-gcc' },
+  avr = { 'avr-gcc' },
   i386 =  { 'i686-gcc' }
 }
 
@@ -113,6 +125,7 @@ local platform_list =
   stm32 = { cpus = { 'STM32F103ZE', 'STM32F103RE' }, arch = 'cortexm' },
   stm32f4 = { cpus =  { 'STM32F401RE', 'STM32F407VG', 'STM32F407ZG' }, arch = 'cortexm' },
   avr32 = { cpus = { 'AT32UC3A0128', 'AT32UC3A0256', 'AT32UC3A0512', 'AT32UC3B0256' }, arch = 'avr32' },
+  avr = { cpus = { 'ATMEGA2560' }, arch = "avr" },
   lpc23xx = { cpus = { 'LPC2368' }, arch = 'arm' },
   lpc24xx = { cpus = { 'LPC2468' }, arch = 'arm' },
   lpc17xx = { cpus = { 'LPC1768' }, arch = 'cortexm' }
@@ -161,4 +174,3 @@ end
 function get_endianness_of_platform( platform )
   return arch_data[ platform_list[ platform ].arch ]
 end
-
